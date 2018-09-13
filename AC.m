71d405258e0512cc0ae95af1e6a78f49334572da
johanna <johannak12@gmail.com>
2018-09-13 17:41:34 +0200

    Add explanatory comments to AC.m

--- AC.m
+++ AC.m
@@ -1,5 +1,21 @@
-% Defines and creates (functions for) the aircraft agents AC
 classdef AC
+    % AC   Summary of AC
+    % This class is meant for initializing aircraft agents and functions
+    % that can be applied to these agents
+    %
+    % AC properties:
+    %   angle        - The agents angle in radials
+    %   position     - The agents [x, y] position
+    %   velocity     - The agent's speed as [x speed, yspeed]
+    %   max_velocity - The agent's maximum speed
+    %   r -          - How far an agent can fly past the lattice until 
+    %                  it will come back on the other side 
+    %
+    % AC Methods:
+    %   AC           - Initialize new AC instance
+    %   move         - Calculates new property values by calling functions
+    %   update       - Move the agent one step forward
+    %   borders      - Makes sure the borders of the lattice wrap around
     
     properties 
        angle 
@@ -10,87 +26,50 @@ classdef AC
     end
     
     methods 
-        % Makes an object/aicraft
         function obj = AC(xpos, ypos, maxv) 
+            % Initializes a new aircraft agent with an initial angle
+            % position, speed and the maximum speed.
             obj.angle = (2*pi).*rand;
             obj.position = [xpos ypos];
             obj.velocity = [cos(obj.angle) sin(obj.angle)];
             obj.max_velocity = maxv;
-            obj.r = 0;
+            obj.r = 0;  %Space around the square that needs to be traversed
+            %before coming out again on the other side.
         end
    
         function obj = move(obj,aircraft)
-        %% What isthe use of this?
-            %Calculates action
-            %vel = obj.separate(aircraft);
-            obj = obj;
-            %obj.velocity = obj.velocity - vel;
-            %obj=obj.apply_force(sep,coh,ali);
+            % Currenlty placeholder, later used to perform function calls
+            % to functions that ensure the aircraft do not collide.
+            % This will most likely be done by altering the velocity in 
+            % such a way that the speed is not affected.
         end
-        %% 
-        % Update the object/aircraft each step
+        
         function obj = update(obj)
+            % Make the agent move one step into its desired direction by
+            % updating its position using the velocity attribute
             obj.position = obj.position + obj.velocity;
         end
         
-        % Define the form of the object? Or the borders of the grid?
         function obj = borders(obj, lattice_size)
-            if obj.position(1) < -obj.r
-                obj.position(1)=lattice_size(1)+obj.r;
+            % Makes sure that the aircraft cannot fall of the grid, by 
+            % connecting opposite sides to eachother.
+            if obj.position(1) < -obj.r %x coordinate < obj.r?
+                obj.position(1)=lattice_size(1)+obj.r; %move to other x end
             end
             
-            if obj.position(2) < -obj.r
-                obj.position(2)=lattice_size(2)+obj.r;
+            if obj.position(2) < -obj.r %y coordinate < obj.r?
+                obj.position(2)=lattice_size(2)+obj.r; %move to other y end
             end
             
-            if obj.position(1) > lattice_size(1) + obj.r
-                obj.position(1)=-obj.r;
+            if obj.position(1) > lattice_size(1) + obj.r %x > latticesize
+                obj.position(1)=-obj.r;     % move to beginning x of board
             end
             
-            if obj.position(2) > lattice_size(2) + obj.r
-                obj.position(2)=-obj.r;
+            if obj.position(2) > lattice_size(2) + obj.r %y > latticesize
+                obj.position(2)=-obj.r;     % move to beginning y of board
             end
-        end
-        
-        
-        function [steer] = separate(obj, ac)
-            desired_separation = 30;
-            steer = [0,0];
-            count = 0;
-            positions = zeros(2,length(ac));       %get ac positions
-            for i=1:1:length(ac)
-                positions(:,i) = ac(i).position;
-            end
-            d = pdist([obj.position; positions']); 
-            d = d(1:length(ac)); %get distances to other aircraft
-            for i=1:1:length(ac)
-                if d(i) > 0 && d(i) <  desired_separation 
-                    if obj.velocity > 0
-                        steer = obj.velocity./2; 
-                    end 
-%                     difference = obj.position - ac(i).position;
-%                     difference = difference./norm(difference);
-%                     difference = difference./d(i);
-%                     steer = steer + difference;
-                    count = count+1;
-                end
-                
-%                 if count > 0
-%                     steer = steer./count;
-%                 end
-%                 
-%                 if norm(steer) > 0
-%                     steer = steer./norm(steer).*obj.max_velocity;
-%                     steer = steer - obj.velocity;
-%                 end
-            end
-        end
-        
+        end        
     end 
     
     
 end 
-
-%% Comments
-% Do we need the apply force function?
-% Do we need the seperation function?
